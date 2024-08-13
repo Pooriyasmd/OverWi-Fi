76cc76102b3c26d82b889e8b08c5360c47a846a7
Pooriya Samadifar <pooriyasmd@gmail.com>
2024-08-13 12:09:03 +0200

    new update

--- Sort.py
+++ Sort.py
@@ -1,8 +1,13 @@
 import numpy as np
+from Functions_of_code import sort_list_ascending
+
 lst = []
 print("Enter 5 numbers to sort:")
 for n in range(1,6):
     ele = int(input())
     lst.append(ele)
-lst = np.sort(lst)
 print(lst)
+sort_list_ascending(lst)
+print(lst)
+        
+print(np.array_split(lst,2))
